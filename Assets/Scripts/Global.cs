547b7af7d5b5d0a931bd577a3213f49c72578f9d
= <2669406432@qq.com>
2019-06-26 21:14:20 +0800

    @Vivid233: smaller head"

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/******All global value define here******/
+public class Global
+{
+    public static Vector3 forward = Vector3.forward;
+    public static Vector3 up = Vector3.up;
+}
